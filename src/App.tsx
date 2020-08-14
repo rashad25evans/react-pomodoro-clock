fdf34ef22c68bfe23cc5c1a7ddc6d2411febb519
Rashad <mkrashad25@gmail.com>
2020-08-14 21:54:42 +0200

    Passed 10 points

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,20 @@ import { render } from "react-dom";
 
 const App: React.FC = () => {
   return (
-    <div>Hello world</div>
+    <div style={{ marginLeft: "600px" }}>
+      <div id="break-label">Break Length</div>
+      <div id="session-label">Session Length</div>
+      <button id="break-decrement">break-decrement</button>
+      <button id="session-decrement">session-decrement</button>
+      <button id="break-increment">break-increment</button>
+      <button id="session-increment">session-increment</button>
+      <button id="break-length">5</button>
+      <button id="session-length">25</button>
+      <button id="timer-label">Session</button>
+      <button id="time-left">mm:ss</button>
+      <button id="start_stop">start</button>
+      <button id="reset">25:00</button>
+    </div>
   );
 }
 
